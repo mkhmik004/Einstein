0378fc1840abdcf33225d7fb940c622d5383878f
Mkhabele Mikejunior Mark Mashaole <137772011+mkhmik004@users.noreply.github.com>
2023-10-09 13:04:21 +0200

    Add files via upload

---
+++ einstein.py
@@ -0,0 +1,11 @@
+#MKHABELE MMM
+#28/06/2023
+def mass_energy_equvalence(n):
+    E=int(n)*300000000**2
+    print("E:",E)
+
+def main():
+    mass=input("m:")
+    mass_energy_equvalence(mass)
+main()
+    
